7eb780b94eb30e3c76d54edad7556e2a8250df73
Samuel Hobel <samuel.hobel@loni.usc.edu>
2018-04-16 22:05:14 -0700

    Initial Commit

---
+++ BusinessSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusinessSearch
+//
+//  Created by Samuel Hobel on 4/16/18.
+//  Copyright © 2018 Samuel Hobel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
